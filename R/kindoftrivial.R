223fce8fadcf2367ff724b268288b24c916fb2b3
Ashley Petersen <ashleyjpete@gmail.com>
2016-07-19 01:19:17 -0700

    added new function, kindoftrivial

---
+++ R/kindoftrivial.R
@@ -0,0 +1,10 @@
+#' A (Kind of) Trivial Function
+#'
+#' Just illustration; nothing really here
+#'
+#' @param x,y two values to be multiplied
+#' @return the product x*y
+#' @examples
+#' trivial(2,3)
+#' @export
+kindoftrivial <- function(x,y) {x * y}
